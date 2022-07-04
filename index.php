070f8d939dd093dd6661e56abc16632c08af3d09
CherylChilly <73657365+JASSIEC@users.noreply.github.com>
2022-07-04 21:29:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>爸爸生日快乐！</title>
+</head>
+
+<body background="生日快乐.gif" style=" background-repeat:no-repeat; background-size:100%; background-attachment:fixed; background-position:center;">
+    <img src="爸爸合照.PNG" width="180" height="220" style="padding-left:5%;">
+    <center>
+        <div style="font-size:64px; font-weight:normal; font-family:华文琥珀; color:#FFD306;">
+            祝爸爸生日快乐！
+        </div>
+        <img src="小熊.gif" width="330" height="350" style="padding-top:1%;padding-left:2%;">
+        <img src="爸爸公主.png" width=" 400" height="400" style="padding-top:1%;padding-left:10%;">
+    </center>
+    <div style="padding-left:20%;padding-top:2%;">
+        <audio autoplay="autoplay" controls="controls" loop="loop" preload="auto" src="祝你生日快乐.mp3">
+        </audio>
+    </div>
+</body>
+
+</html>
